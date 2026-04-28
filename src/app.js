5977fa39065f6e4593c6b48008b03fe452fbfc32
akshit1715 <akshitshuklaofficial1@gmail.com>
2026-04-28 18:00:46 +0530

    add register controller and bug fixes

--- src/app.js
+++ src/app.js
@@ -10,6 +10,20 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.get("/",(req,res)=>{
+    res.send("Hello World mongoDB connection is successful")
+})
+
+
+
+//routes
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+// http://localhost:8000/api/v1/users/register
 
 
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
  import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 });
-connectDB();
-
-then(()=>{
+connectDB()
+.then(()=>{
     app.listen(process.env.PORT ||8000,() =>{
-        console.log(' server is running at port:${process.env.PORT}');
+        console.log(` server is running at port:${process.env.PORT}`);
     } )
 })
 .catch((err)=>{
